78860f89e6a8b9697623e84b24f6ca330f6f6ecc
Ilshat Galin <ilshat@publicsonar.com>
2023-12-09 11:42:08 +0500

    Revert "Implemented http server and router"
    This reverts commit d16865540748a83d8e97e08e494565b55e9a1798.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"loyalty-system/internal/config"
-	"loyalty-system/internal/router"
-	"loyalty-system/internal/server"
 )
 
 func main() {
-	cfg := config.New()
-	router := router.New(cfg)
-
-	httpServer := server.New(cfg.SrvAddr, router)
-	if err := httpServer.Run(); err != nil {
-		panic(err)
-	}
+	config.New()
 }
