8e2138d139d10f2ab9a955165e87912a368d9387
Ilshat Galin <ilshat@publicsonar.com>
2023-12-20 23:39:17 +0500

    Revert "Merge pull request #8 from Galish/refactoring"
    This reverts commit a489203a1b01aa1259314e78fa422e59de5aa9ad, reversing
    changes made to 7e6d9c6a044a31864382fcc42932d1285defd248.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,34 +1,3 @@
 package main
 
-import (
-	"github.com/Galish/loyalty-system/internal/auth"
-	"github.com/Galish/loyalty-system/internal/config"
-	"github.com/Galish/loyalty-system/internal/logger"
-	"github.com/Galish/loyalty-system/internal/loyalty"
-	"github.com/Galish/loyalty-system/internal/repository/psql"
-	"github.com/Galish/loyalty-system/internal/router"
-	"github.com/Galish/loyalty-system/internal/server"
-)
-
-func main() {
-	cfg := config.New()
-
-	logger.Initialize(cfg.LogLevel)
-
-	store, err := psql.NewStore(cfg)
-	if err != nil {
-		panic(err)
-	}
-	defer store.Close()
-
-	authService := auth.NewService(store, cfg.SecretKey)
-	loyaltyService := loyalty.NewService(store, cfg)
-	defer loyaltyService.Close()
-
-	router := router.New(cfg, authService, loyaltyService)
-
-	httpServer := server.New(cfg.SrvAddr, router)
-	if err := httpServer.Run(); err != nil {
-		panic(err)
-	}
-}
+func main() {}
