3fb67988bf13fd4dd90ca2524a3702ffe0dfd514
Ilshat Galin <ilshat@publicsonar.com>
2023-12-21 12:52:42 +0500

    Fixed vet test warnings

--- internal/balance/service.go
+++ internal/balance/service.go
@@ -2,14 +2,13 @@ package balance
 
 import (
 	"github.com/Galish/loyalty-system/internal/repository"
-	repo "github.com/Galish/loyalty-system/internal/repository"
 )
 
 type BalanceService struct {
 	repo repository.BalanceRepository
 }
 
-func NewService(repo repo.BalanceRepository) *BalanceService {
+func NewService(repo repository.BalanceRepository) *BalanceService {
 	return &BalanceService{
 		repo: repo,
 	}
--- internal/order/service.go
+++ internal/order/service.go
@@ -2,14 +2,13 @@ package order
 
 import (
 	"github.com/Galish/loyalty-system/internal/repository"
-	repo "github.com/Galish/loyalty-system/internal/repository"
 )
 
 type OrderService struct {
 	repo repository.OrderRepository
 }
 
-func NewService(repo repo.OrderRepository) *OrderService {
+func NewService(repo repository.OrderRepository) *OrderService {
 	return &OrderService{
 		repo: repo,
 	}
